17f0e85f7b69404403458fa468e3d89064a533bb
KFR1230 <su0916111448@gmail.com>
2023-11-01 17:47:04 +0800

    fixed// button and mouse destance

--- src/components/basic/MenuButton.jsx
+++ src/components/basic/MenuButton.jsx
@@ -4,7 +4,7 @@ const MenuButton = () => {
   const navigate = useNavigate();
   return (
     <>
-      <div className="transform transition-all mt-2 w-[6.5rem] h-[2.5rem] sm:w-32 sm:h-[3.5rem] lg:w-64 lg:h-24 ">
+      <div className="transform transition-all my-2 w-[6.5rem] h-[2.5rem] sm:w-32 sm:h-[3.5rem] lg:w-64 lg:h-24 ">
         <button
           className="w-full h-full rounded-xl sm:py-2 sm:text-2xl lg:text-5xl  sm:rounded-2xl bg-lime-900 text-slate-100 font-bold shadow-outer-b hover:bg-lime-600 active:bg-lime-900"
           onClick={() => navigate('/menu')}
