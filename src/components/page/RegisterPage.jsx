f27591ba298bb8872d2631fec4936534b5dde66e
KFR1230 <su0916111448@gmail.com>
2023-12-15 16:22:40 +0800

    fixed//#root height and min-height, login page, regist page height

--- src/components/page/RegisterPage.jsx
+++ src/components/page/RegisterPage.jsx
@@ -57,8 +57,8 @@ function RegisterPage() {
       <div
         className={
           isImageLoaded
-            ? `flex min-h-full flex-1 flex-col px-6 py-12 lg:px-8 overflow-y-auto h-16 bg-[url(@/assets/image/paper1920.jpg)]`
-            : `flex min-h-full flex-1 flex-col px-6 py-12 lg:px-8 overflow-y-auto h-16 bg-[#d6b892]`
+            ? `flex h-svh flex-1 flex-col px-6 py-12 lg:px-8 overflow-y-auto bg-[url(@/assets/image/paper1920.jpg)]`
+            : `flex h-svh flex-1 flex-col px-6 py-12 lg:px-8 overflow-y-auto bg-[#d6b892]`
         }
       >
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
